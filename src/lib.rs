a139f0f412fd340c792227433f7f7b59a7c7dfb3
Michael Bachmann <mbachmann@bbmsoft.net>
2026-09-18 12:13:38 +0200

    added CancelOn trait

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 #[cfg(feature = "mpsc_multiplexer")]
 #[doc(hidden)]
 pub use paste;
+use tokio::select;
 
 #[cfg(feature = "mpsc_multiplexer")]
 mod mpsc_multiplexer;
@@ -8,3 +9,20 @@ mod mpsc_multiplexer;
 pub mod trace_collector;
 #[cfg(feature = "while_select")]
 mod while_select;
+
+pub trait CancelOn {
+    type Output;
+    fn or_cancel_on(self, cancel: impl Future) -> impl Future<Output = Option<Self::Output>>;
+}
+
+impl<F: Future> CancelOn for F {
+    type Output = F::Output;
+
+    async fn or_cancel_on(self, cancel: impl Future) -> Option<Self::Output> {
+        select! {
+            biased;
+            _ = cancel => None,
+            output = self => Some(output),
+        }
+    }
+}
